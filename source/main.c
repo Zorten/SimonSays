0daa7ec9f582a08cc882577452040fbc83d3aadb
Zergio Ruvalcaba <zergionaruto@hotmail.com>
2021-06-04 21:38:47 -0700

    Can't get the game to restart cuz of failure; can't get past level 2 as it stops taking input

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 #include "../header/scheduler.h"
 #include "stdbool.h"
 
-bool start = false; //variable to determine whether game has started or not
+static bool start = false; //variable to determine whether game has started or not
 enum Menu_States {waitOn, waitOff, input} Menu_state;
 void TickFct_Menu(){ //Function will flash all 4 quarters as long as there's no input. Aka the game hasn't begun.
     char buttons = ~PINA;
@@ -58,17 +58,15 @@ void TickFct_Menu(){ //Function will flash all 4 quarters as long as there's no 
     }
 }
 
+const unsigned char levels = 3;
 char Simon[3] = {1, 4, 3};
 static unsigned char round = 0;
-enum Game_States {sequence};
-int TickFct_Game(int state){
-
-	//static unsigned char column = 0x00; 
-	//static unsigned char row = 0x00; 
+enum Game_States {sequence} Game_state;
+void TickFct_Game(){ 
 	unsigned char quarter = 0;
 	unsigned long loop = 0;
 	
-	switch (state) {
+	switch (Game_state) {
 		case sequence:
 			for (unsigned char i = 0; i < round; ++i){
 				quarter = Simon[i];
@@ -136,53 +134,64 @@ int TickFct_Game(int state){
 						loop = 0;
 						break;
 				}
-				//PORTC = column;
-				//PORTD = ~row;
 			}
-	}
-
-	return state; 
+	} 
 }
 
-enum Input_states {init, wait, check};
-int TickFct_Input(int state){
+static unsigned char correctPresses = 0;
+unsigned char pressCount = 0;
+bool inputDone = false;
+enum Input_states {init, wait, check, done} Input_state;
+void TickFct_Input(){
+	unsigned long loop = 0;
 	char buttons = ~PINA;
 	unsigned char press = 0;
-	switch (state){
+	switch (Input_state){
 		case init:
 			PORTC = 0x00;
 			PORTD = ~(0x00);
-			state = wait;
+			Input_state = wait;
 			break;
 
 		case wait:
+			PORTC = 0x00;
+			PORTD = ~(0x00);
 			if (buttons == 0x00){
-				state = wait; 			
+				Input_state = wait; 			
 			}
 			else if (buttons == 0x01){
-				state = check;
+				Input_state = check;
 				press = 1;
 			}
 			else if (buttons == 0x02){
-				state = check;
+				Input_state = check;
 				press = 2;
 			}
 			else if (buttons == 0x04){
-				state = check;
+				Input_state = check;
 				press = 3;
 			}
 			else if (buttons == 0x08){
-				state = check;
+				Input_state = check;
 				press = 4;
 			}
 			break;
 
 		case check:
-			
+			if (pressCount < round){
+				Input_state = wait;
+			}
+			else{
+				Input_state = done;
+			}
+			break;
+
+		case done:
+			Input_state = wait;
 			break;
 	}
 
-	switch (state){
+	switch (Input_state){
 		case init:
 			break;
 
@@ -190,6 +199,48 @@ int TickFct_Input(int state){
 			break;
 
 		case check:
+			++pressCount;
+			if (press == 1){
+				while (loop < 50000){
+					PORTC = 0xE0;	
+					PORTD = ~(0x03);
+					++loop;
+				}
+				loop = 0;
+			}
+			else if (press == 2){
+				while (loop < 50000){
+					PORTC = 0x07;	
+					PORTD = ~(0x03);
+					++loop;
+				}
+				loop = 0;
+			}
+			else if (press == 3){
+				while (loop < 50000){
+					PORTC = 0xE0;	
+					PORTD = ~(0x18);
+					++loop;
+				}
+				loop = 0;
+			}
+			else if (press == 4){
+				while (loop < 50000){
+					PORTC = 0x07;	
+					PORTD = ~(0x18);
+					++loop;
+				}
+				loop = 0;
+			}
+
+
+			if (press == Simon[pressCount - 1]){
+				++correctPresses;
+			}
+			break;
+
+		case done: 
+			inputDone = true;
 			break;
 	}
 }
@@ -202,70 +253,70 @@ int main(void) {
     	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
 
-    //Setting up task scheduler
-	task tasks[1];
-	const unsigned short numTasks = 1;
-	const unsigned long tasksPeriodGCD = 100;
-	const unsigned long periodGame = 1000;
 
-/*
-	const unsigned long periodTopRight = 500;
-	const unsigned long periodBottomLeft = 500;
-	const unsigned long periodBottomRight = 500;
-*/
-	
-	unsigned char t = 0;
- 
-	tasks[t].state = sequence;
-	tasks[t].period = periodGame;
-	tasks[t].elapsedTime = tasks[t].period;
-	tasks[t].TickFct = &TickFct_Game;
-	++t;
+	unsigned long Menu_elapsedTime = 100;
+    	//unsigned long Game_elapsedTime = 1000;
+    	//unsigned long Input_elapsedTime = 100;
 
-/*
-	tasks[t].state = hold2;
-	tasks[t].period = periodTopRight;
-	tasks[t].elapsedTime = tasks[t].period;
-	tasks[t].TickFct = &TickFct_TopRight;
-	++t;
-	tasks[t].state = hold3;
-	tasks[t].period = periodBottomLeft;
-	tasks[t].elapsedTime = tasks[t].period;
-	tasks[t].TickFct = &TickFct_BottomLeft;
-	++t;
-	tasks[t].state = hold4;
-	tasks[t].period = periodBottomRight;
-	tasks[t].elapsedTime = tasks[t].period;
-	tasks[t].TickFct = &TickFct_BottomRight;
-	++t;
-*/
+
+	const unsigned char timerPeriod = 50;
+
+
 	//Set the timer and turn it on
-	TimerSet(tasksPeriodGCD);
+	TimerSet(timerPeriod);
 	TimerOn();
-	
-	
-	Menu_state = waitOn; //initial state for menu 
-    	unsigned short i; //scheduler for-loop iterator
+
+	Menu_state = waitOn; //initial state for menu
+	Game_state = sequence;
+	Input_state = init;
+		
+
+	unsigned long j = 0;
+	bool gameOver = false; 
 	while (1) {
         	if (!start){ //menu function independent as it should only run when the game isn't being played
-            		TickFct_Menu();
+			if (Menu_elapsedTime >= 100){
+            			TickFct_Menu();
+				Menu_elapsedTime = 0;
+			}
         	}
         	else{
-			round = 1;
-			for (i = 0; i < numTasks; i++){
-				if (tasks[i].elapsedTime >= tasks[i].period){
-				    tasks[i].state = tasks[i].TickFct(tasks[i].state);
-				    tasks[i].elapsedTime = 0;
+				round = 1;
+				while (!gameOver){
+					correctPresses = 0;
+					TickFct_Game();
+					while (!inputDone){
+						if (j >= 1000){
+							TickFct_Input();
+							j = 0;	
+						}
+						++j;
+					}
+					inputDone = false; 
+
+					if (correctPresses == round){
+						if (round == levels){
+							gameOver = true;
+							start = false;
+						}
+						else{
+							++round;
+						}
+					}
+					else{
+						gameOver = true;
+						start = false;
+					}
 				}
-				tasks[i].elapsedTime += tasksPeriodGCD;
-		    	}
         	}
-	
+
+		
 		while(!TimerFlag){};
 		TimerFlag = 0;
+		Menu_elapsedTime += timerPeriod;
+
 	}
 	
 	return 1;
 }
-
 
